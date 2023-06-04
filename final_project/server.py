4291b029bb1f0cbb075294b975b5f2f9ff07c2b3
clemtine <clementineamevor97@gmail.com>
2023-06-03 20:03:01 -0400

    Final changes

--- final_project/server.py
+++ final_project/server.py
@@ -21,6 +21,7 @@ def frenchToEnglish():
 @app.route("/")
 def renderIndexPage():
     # Write the code to render template
+    return "Hello World"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
